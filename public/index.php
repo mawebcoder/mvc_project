92c3fcb3204df836d3bfb8b8c52e60bcc4018b32
mohammad <mawebcoder@gmail.com>
2023-01-09 23:37:01 +0330

    add 404 page

--- public/index.php
+++ public/index.php
@@ -1,8 +1,9 @@
 <?php
-
+ini_set('display_errors',1);
 
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 
 require_once __DIR__ . "/../bootstrap/app.php";
+
--- system/Routing.php
+++ system/Routing.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace System;
+
 
 class Routing
 {
@@ -57,7 +57,7 @@ class Routing
         }
 
         if (!isset($_POST['_method'])) {
-            return $httpVerb;
+            return self::HTTP_VERB_POST;
         }
 
         return match (strtolower($_POST['_method'])) {
@@ -70,5 +70,13 @@ class Routing
     private function getRequestedRouteParameters(): array
     {
         return [];
+    }
+
+    public function error404()
+    {
+        http_response_code(404);
+
+        require_once __DIR__.'/../resources/views/errors/404.php';
+        exit();
     }
 }
